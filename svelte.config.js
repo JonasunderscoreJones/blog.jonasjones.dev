9a10df78c81f30ec3a351398fa95cdf86e6346fa
J-onasJones <me@jonasjones.dev>
2024-06-07 01:06:21 +0200

    please be fixed now

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,13 @@
-import preprocess from 'svelte-preprocess';
-import cloudflare from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-cloudflare';
 
-export default {
-  preprocess: preprocess(),
-  kit: {
-    adapter: cloudflare(),
-    target: '#svelte'  // Ensure this matches your target in `src/app.html`
-  }
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
+
+export default config;
