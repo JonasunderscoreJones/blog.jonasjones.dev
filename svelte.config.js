6df7c8932ab8accc38a713a8788b282c8ca8d66f
J-onasJones <me@jonasjones.dev>
2024-06-07 00:57:14 +0200

    maybe fixed cf again

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import adapter from '@sveltejs/adapter-cloudflare';
+import preprocess from 'svelte-preprocess';
+import cloudflare from '@sveltejs/adapter-cloudflare';
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-
+  preprocess: preprocess(),
   kit: {
-		adapter: adapter(),
-	}
-}
+    adapter: cloudflare(),
+    target: '#svelte'  // Ensure this matches your target in `src/app.html`
+  }
+};
