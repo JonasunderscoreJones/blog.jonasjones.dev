6fa354d1d8a11c1099c12c8c8ed21dbc7fab9f5d
J-onasJones <me@jonasjones.dev>
2024-06-15 22:36:33 +0200

    cleaned up routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,11 @@
 import Home from './routes/Home.svelte';
-import Loading from './routes/Loading.svelte';
 import NotFound from './routes/NotFound.svelte';
-import wrap from 'svelte-spa-router/wrap';
 import Dashboard from './routes/Dashboard.svelte';
+import Post from './routes/Post.svelte';
 
 export default {
     '/': Home,
-    '/post/:year/:month/:day/:title': wrap({
-        // Note that this is a function that returns the import
-        asyncComponent: () => import('./routes/Post.svelte'),
-        // Show the loading component while the component is being downloaded
-        loadingComponent: Loading,
-        // Pass values for the `params` prop of the loading component
-        loadingParams: {
-            message: 'Loading the Name route…'
-        }
-    }),
+    '/post/:year/:month/:day/:title': Post,
     '/dash': Dashboard,
     '*': NotFound
 };
