0f6051f834323b420575732b8cb65735352b0838
J-onasJones <me@jonasjones.dev>
2024-06-16 20:54:14 +0200

    added RSS feed link

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -5,6 +5,9 @@
 </script>
 
 <div>
+	<div class="rss">
+		<a href="https://rss.jonasjones.dev/blog">Susbcribe to the RSS Feed <i class="fas fa-rss"></i></a>
+	</div>
 	<h1>--- Latest Blog Posts ---</h1>
 </div>
 
@@ -16,5 +19,10 @@
 		font-size: 2em;
 		margin-top: 50px;
 		text-align: center;
+	}
+
+	.rss a {
+		text-decoration: none;
+		color: inherit;
 	}
 </style>
