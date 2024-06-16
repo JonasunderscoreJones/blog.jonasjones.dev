8512e24c8dff138f1a771a6b84068955ccfac410
J-onasJones <me@jonasjones.dev>
2024-06-16 20:07:53 +0200

    Changed Home page

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,3 +1,20 @@
+<script lang="ts">
+	import PostList from "../components/PostList.svelte";
+
+	export let searchquery: string = "";
+</script>
+
 <div>
-	<h1>Welcome to my Blog!</h1>
+	<h1>--- Latest Blog Posts ---</h1>
 </div>
+
+<PostList query={searchquery} />
+
+<style>
+	h1 {
+		font-family: 'JetBrains Mono', monospace;
+		font-size: 2em;
+		margin-top: 50px;
+		text-align: center;
+	}
+</style>
